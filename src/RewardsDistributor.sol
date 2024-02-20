ae5e7ce590da428172296fd4d1f789a78a35cea7
0xMariano <104645597+0xMariano@users.noreply.github.com>
2024-02-20 14:02:52 +0100

    Update RewardsDistributor.sol

--- src/RewardsDistributor.sol
+++ src/RewardsDistributor.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITokenContract {
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+interface INFTContract {
+    function mint(address recipient, string calldata tokenURI) external returns (uint256);
+}
+
+contract RewardDistributor {
+    ITokenContract public rewardToken;
+    INFTContract public contributionNFT;
+
+    constructor(address _rewardTokenAddress, address _contributionNFTAddress) {
+        rewardToken = ITokenContract(_rewardTokenAddress);
+        contributionNFT = INFTContract(_contributionNFTAddress);
+    }
+
+    function distributeTokenReward(address recipient, uint256 amount) external {
+        require(rewardToken.transfer(recipient, amount), "Token transfer failed");
+    }
+
+    function distributeNFTReward(address recipient, string calldata tokenURI) external {
+        contributionNFT.mint(recipient, tokenURI);
+    }
+
+   
+}
 
